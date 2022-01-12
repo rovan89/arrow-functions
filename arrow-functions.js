44fcbe41112cec30cb80cfe52bb0cdc62067628a
rovan89 <devireland89@gmail.com>
2022-01-12 13:59:10 +0000

    Add first two functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,8 +4,20 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     // Code block
+//     return a + b
+// }
+// let sum = addTwoNumbers(3,5);
+// console.log(sum)
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b
+}
+let sum = addTwoNumbers(3,5);
+console.log(sum)
 
 // Single Line Arrow Function With Parameters
 
